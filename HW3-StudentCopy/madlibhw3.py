dbe0f367fae102c8bc82a6ee58607937773348dc
Olivia Withers <oliviawithers@Olivias-MacBook-Pro-3.local>
2016-11-10 09:54:37 -0500

    changes to mad lib

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -5,11 +5,15 @@
 # 1) Only use the first 150 tokens
 # 2) Pick 5 parts of speech to prompt for, including nouns
 # 3) Replace nouns 15% of the time, everything else 10%
-
+#2 Edits 
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
-print("START*******")
+import nltk
+import random 
 
+from nltk import word_tokenize,sent_tokenize
 
-print("\n\nEND*******")
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective","RB":"an adverb"}
+substitution_probabilities = {"NN":.15,"NNS":.15,"VB":.10,"JJ":.10,"RB":.10}
+
