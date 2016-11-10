13bd02201f20a1d513eaee308a1fc4b781a0224a
Olivia Withers <oliviawithers@Olivias-MacBook-Pro-3.local>
2016-11-09 20:45:06 -0500

    twitter 3a is complete

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -1,8 +1,24 @@
+import tweepy
+import os
+
 # Write a Python file that uploads an image to your 
 # Twitter account.  Make sure to use the 
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+tweet= "#UMSI-206 #Proj3"
+filename= os.path.abspath('pizza.jpg')
+status= api.update_with_media(filename, status=tweet)
+
+
+
+
 
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
